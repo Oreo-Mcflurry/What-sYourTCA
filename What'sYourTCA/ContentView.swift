20d0cfca199226119d5fb1b090445212cee0a757
유인호 <yuiop1029@naver.com>
2024-05-26 21:06:03 +0900

    Initial Commit

---
+++ What'sYourTCA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  What'sYourTCA
+//
+//  Created by A_Mcflurry on 5/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ What'sYourTCA/What_sYourTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  What_sYourTCAApp.swift
+//  What'sYourTCA
+//
+//  Created by A_Mcflurry on 5/26/24.
+//
+
+import SwiftUI
+
+@main
+struct What_sYourTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
